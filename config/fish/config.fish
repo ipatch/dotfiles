206ac48c0acea75d02f2fc6c71e38f890332a186
ipatch <chris.r.jones.1983@gmail.com>
2017-01-03 21:00:09 -0600

    Create directory structure for neovim and begin editing the fish configuration file, config.fish

---
+++ config/fish/config.fish
@@ -0,0 +1,24 @@
+# User specified file for configuring the fish shell.
+# Author: Chris Jones
+# Contact: @truckmonth chris.r.jones.1983@gmail.com
+
+# set the size of the command history for fish
+# Note - fish supports 256K unique commands in history,
+# SEE - https://github.com/fish-shell/fish-shell/issues/2674
+
+
+# Command aliases
+alias l='ls -lah'
+alias cp='cp -iv'
+alias rm='rm -iv'
+alias mv='mv -iv'
+
+# Command aliases for git projects
+alias gs='git status'
+alias gd='git diff --stat'
+alias ga='git add --all'
+alias gp='git push'
+
+# Command aliases
+alias df='df -h'
+alias dotfiles="cd /opt/Code/dotfiles"
