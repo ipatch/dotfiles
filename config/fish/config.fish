5ee3a36d084b28c0bebdd5c99f41ed9853f20723
ipatch <chris.r.jones.1983@gmail.com>
2017-12-15 18:36:30 -0600

    hmmm

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -26,13 +26,22 @@ alias gp='git push'
 # Command aliases
 alias df='df -h'
 alias du='du -h'
-alias dotfiles="cd /opt/Code/dotfiles"
+alias dotfiles='cd /opt/Code/dotfiles'
 
 # Get asdf package manager working with fish shell.
 source ~/.asdf/asdf.fish
 
 # Configuration required for Ruby Version Manager
 # rvm default
+
+# ==============================================================================
+
+# USER defined environment variables
+set -x HOSTNAME (hostname -s)
+
+
+
+# ==============================================================================
 
 # Add rust-lang local bin directory to PATH for working with cargo.
 set -x PATH $PATH $HOME/.cargo/bin
@@ -72,25 +81,45 @@ switch $ERL_VER
   case '20'
     set -x ERL_AFLAGS '-kernel shell_history enabled'
   case '*'
-    echo OTP v20 is not present on this system.
+    echo OTP v20 is not present on this system.;
 end
 
 # Setup OS specific environment variables
 # DOC: http://fishshell.com/docs/current/tutorial.html#tut_conditionals
 switch (uname)
   case Linux
-    set -x PATH $PATH /home/linuxbrew/.linuxbrew/bin
-    set -x MANPATH $MANPATH /home/linuxbrew/.linuxbrew/share/man
-    set -x INFOPATH $INFOPATH /home/linuxbrew/.linuxbrew/share/info
+    set -x PATH $PATH/home/linuxbrew/.linuxbrew/bin
+    set -x MANPATH $MANPATH/home/linuxbrew/.linuxbrew/share/man
+    set -x INFOPATH $INFOPATH/home/linuxbrew/.linuxbrew/share/info
   case Darwin
     alias pg-start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
     alias pg-stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
 
     # Setup OS specific PATH variables for macOS
 
-    # Add the below to the path in order to get react-native CLI working.
+    # Add the below path in order to get react-native CLI working.
     set -x PATH $PATH $HOME/Library/Android/sdk/platform-tools
     set -x PATH $PATH $HOME/anaconda2/bin
     set -x PATH $PATH $HOME/.config/yarn/global/node_modules/.bin
     set -x PATH $PATH $HOME/bin
+
+    # Check if the asdf bin exists
+    if type -q asdf
+      # 1) List installed plugins via asdf version manager
+      # 2) put the above output into a file.
+      asdf plugin-list > /opt/Code/dotfiles/asdf/.tool-versions.$HOSTNAME.$USER;
+      ln -sf /opt/Code/dotfiles/asdf/.tool-versions.$HOSTNAME.$USER $HOME/.tool-versions;
+      echo -e "Successfully generated your .tool-versions file in $HOME"\n"\
+and it is linked to /opt/Code/dotfiles/asdf/.tool-versions.$HOSTNAME.$USER"
+    else
+      echo asdf is not installed on this system.
+    end
+
+
+
+    # List installed versions
+    asdf list
+
+    # NOTE: to access an individual item of list
+    # echo $PATH[1]
 end
