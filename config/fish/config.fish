6b7967a6bf5a92b2f16a4dedefed68e6916680e2
ipatch <chris.r.jones.1983@gmail.com>
2017-12-25 22:48:58 -0600

    (fortune)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -59,7 +59,7 @@ alias sudo='sudo -E'
 alias gs='git status'
 alias gd='git diff --stat'
 alias ga='git add --all'
-alias gc='git commit -m (fortune)'
+alias gc='git commit -m \"(fortune)\"'
 alias gp='git push'
 
 # Get asdf package manager working with fish shell.
