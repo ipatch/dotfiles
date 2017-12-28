7557914518aa3f8a10133dc69f1f03c798b517ef
ipatch <chris.r.jones.1983@gmail.com>
2017-12-28 03:14:07 -0600

    For some reason a glaze passes over people's faces when you say "Canada".  Maybe we should invade South Dakota or something. 		-- Sandra Gotlieb, wife of the Canadian ambassador to the U.S.

--- config/fish/interactive.fish
+++ config/fish/interactive.fish
@@ -3,7 +3,7 @@ if test -f $HOME/.config/fish/abbreviations.fish
   source $HOME/.config/fish/abbreviations.fish
 end
 
-if test -f $HOME/.config/.asdf/asdf.fish
+if test -f $HOME/.asdf/asdf.fish
   # Get asdf package manager working with fish shell.
   source $HOME/.asdf/asdf.fish
 end
