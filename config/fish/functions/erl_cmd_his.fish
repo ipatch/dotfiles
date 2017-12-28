16436cfac755c43a682e1634a8eb70c445446a9a
ipatch <chris.r.jones.1983@gmail.com>
2017-12-28 02:34:04 -0600

    Now hatred is by far the longest pleasure; Men love in haste, but they detest at leisure. 		-- George Gordon, Lord Byron, "Don Juan"

---
+++ config/fish/functions/erl_cmd_his.fish
@@ -0,0 +1,17 @@
+function erl_cmd_his --description 'enable shell_history for erl / iex if >= 20'
+  # cheers 🍺 for tr 😎
+  # return the current version of erlang / OTP installed on the local system.
+  # NOTE: uncomment the below command to see diagnostic 🚕 info about the env var.
+  # set -S ERL_VER
+  set -x ERL_VER (erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell\
+   | tr -d \r'"')
+
+  # Enable shell history for elixir / erlang
+  # Ref: https://stackoverflow.com/questions/9560815/
+  switch $ERL_VER
+    case '20'
+      set -x ERL_AFLAGS '-kernel shell_history enabled'
+    case '*'
+      # echo OTP v20 is not present on this system.;
+  end
+end
---
+++ config/fish/functions/mk_asdf_bins.fish
@@ -0,0 +1,25 @@
+function mk_asdf_bins --description 'build `.tool-versions`, then ln $HOME'
+  # Check if the asdf binary exists
+  if type -q asdf
+    rm -f $DOTFILES/asdf/.tool-versions.$HOSTNAME.$USER > /dev/null
+    # 1) List installed plugins & version numbers via asdf version manager
+    # 2) put the above output into a file.
+    for x in (asdf plugin-list)
+      # echo $x (asdf list $x | tail -n1) >> /opt/Code/dotfiles/asdf/.tool-versions.$HOSTNAME.$USER
+      # CREDIT 💳: @Thom via Gitter
+      echo $x (asdf list $x)[-1] >> $DOTFILES/asdf/.tool-versions.$HOSTNAME.$USER
+    end
+    ln -sf $DOTFILES/asdf/.tool-versions.$HOSTNAME.$USER $HOME/.tool-versions;
+    # echo -e "Successfully generated your .tool-versions file in $HOME"\n"\
+    # and it is linked to /opt/Code/dotfiles/asdf/.tool-versions.$HOSTNAME.$USER"
+
+    # List the latest version of Python installed in the $HOME directory via asdf
+    # set -x PYTHON_LATEST (ls ~/.asdf/installs/python/ | tail -n1)
+    set -x PYTHON_LATEST (ls ~/.asdf/installs/python/)[-1]
+    # Add the latest version of Python to the PATH
+    set -x PATH $PATH ~/.asdf/installs/python/$PYTHON_LATEST/bin
+
+  else
+    # echo asdf is not installed on this system.
+  end
+end
