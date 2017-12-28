86e69e29580aa52bfa3b3b415019ff4ef94e82de
ipatch <chris.r.jones.1983@gmail.com>
2017-12-28 01:40:27 -0600

    Auction: 	A gyp off the old block.

---
+++ config/fish/abbreviations.fish
@@ -0,0 +1,44 @@
+###
+# A place to store abbreviations for making things a tad bit quicker.
+###
+
+###
+# Added the below abbreviations because I'm really tired of typing out the below
+# command.
+###
+abbr -a editfish 'nvim $HOME/.config/fish/config.fish'
+abbr -a editnvim 'nvim $HOME/.config/nvim/init.vim'
+abbr -a l 'ls -lah'
+abbr -a cp 'cp -iv'
+abbr -a rm 'rm -iv'
+abbr -a mv 'mv -iv'
+abbr -a df 'df -h'
+abbr -a du 'du -h'
+abbr -a dotfiles 'cd /opt/Code/dotfiles'
+abbr -a hmmm 'echo "¯\_(ツ)_/¯"'
+# preserve the $USER environment when running the sudo command.
+abbr -a sudo 'sudo -E'
+###
+# git abbreviations
+###
+# Command aliases for git projects
+abbr -a gs 'git status'
+abbr -a gd 'git diff --stat'
+abbr -a ga 'git add --all'
+# NOTE: see the functions dir for how to issue quick commits, ie. `gc`
+abbr -a gp 'git push'
+
+switch (uname)
+  case Darwin
+    abbr -a pg-start "launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+    abbr -a pg-stop "launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+end
+
+# Add special 🚌 aliases if certain binaries are found.
+if type -q pycp
+  abbr -a cp 'pycp -i'
+end
+
+if type -q ccat
+  abbr -a cat 'ccat'
+end
