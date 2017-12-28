0fccc26577808fd1b2bcef53cca5c1aba3c260ed
ipatch <chris.r.jones.1983@gmail.com>
2017-12-28 01:40:27 -0600

    Auction: 	A gyp off the old block.

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+
+
+
+if status --is-interactive
+  source $HOME/.config/fish/interactive.fish
+  # Base16 Shell - a sane colorscheme for better shell colors 🌈
+  eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
+end
