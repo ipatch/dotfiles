0c17f352d49cff823d02a230c7029edf87d1d219
ipatch <chris.r.jones.1983@gmail.com>
2018-01-01 22:45:28 -0600

    I've been on a diet for two weeks and all I've lost is two weeks. 		-- Totie Fields

--- config/fish/functions/toggle_asdf_shims_and_bins.fish
+++ config/fish/functions/toggle_asdf_shims_and_bins.fish
@@ -1,13 +1,13 @@
 function toggle_asdf_shims_and_bins --description 'managing asdf bins / shims in a sane way!'
   # check to see if asdf_pythons are in the PATH
-  if set -l index (contains -i /Users/capin/.asdf/shims $PATH)
+  if set -l index (contains -i $HOME/.asdf/shims $PATH)
     set -e PATH[$index]
     echo removed asdf shims from PATH
   else
     set -x PATH $HOME/.asdf/shims $PATH
     echo added asdf shims to PATH
   end
-  if set -l index (contains -i /Users/capin/.asdf/bin $PATH)
+  if set -l index (contains -i $HOME/.asdf/bin $PATH)
     set -e PATH[$index]
     echo removed asdf shims and bins from PATH
   else
