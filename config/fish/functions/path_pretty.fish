4295c8b55cbddfe8c91c31dc3694d9466a10dbdf
ipatch <chris.r.jones.1983@gmail.com>
2018-01-02 00:07:26 -0600

    add path_pretty func to fish

---
+++ config/fish/functions/path_pretty.fish
@@ -0,0 +1,12 @@
+function path_pretty --description 'print a pretty listing of the $PATH'
+  # 💳 https://github.com/fish-shell/fish-shell/issues/2639#issuecomment-301896209
+  echo \$PATH
+  echo $PATH | tr " " "\n" | nl
+
+  echo \$fish_user_paths
+  echo $fish_user_paths | tr " " "\n" | nl
+
+  # PATH entries can be removed with the below commands
+  # set --erase PATH[1]
+  # set -e PATh[4]
+end
