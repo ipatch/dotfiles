ba9f5941bd42b88ee55a46389cd35ccfc4456ed8
ipatch <chris.r.jones.1983@gmail.com>
2018-01-08 00:55:13 -0600

    Arnold's Addendum: 	Anything not fitting into these categories causes cancer in rats.

--- config/fish/functions/rvm_toggle.fish
+++ config/fish/functions/rvm_toggle.fish
@@ -1,10 +1,10 @@
 function rvm_toggle --description 'toggle rvm if found'
   set ruby_ver eval (which ruby)
   # switch ($ruby_ver)
-  if string match -q -- "*.rvm*" $ruby_ver #and test -d $HOME/.rvm;
+  if string match -q -- "*.rvm*" $ruby_ver and test -d $HOME/.rvm
     echo "Now switch back to system ruby"
     rvm use system
-  else if string match -q --invert "*.rvm*" $ruby_ver #and test -d $HOME/.rvm;
+  else if string match -q --invert "*.rvm*" $ruby_ver and test -d $HOME/.rvm
     rvm default
   else
     echo "rvm not found in \$PATH"
