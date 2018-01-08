a5ee0d826f5f04c89436a954fe54803498ff757c
ipatch <chris.r.jones.1983@gmail.com>
2018-01-07 23:35:37 -0600

    have a somewhat working `rvm-toggle function`

--- config/fish/functions/rvm_toggle.fish
+++ config/fish/functions/rvm_toggle.fish
@@ -1,17 +1,9 @@
 function rvm_toggle --description 'toggle rvm if found'
-  # check if `.rvm` exists in $HOME
-  if test -d $HOME/.rvm
-    if string match -qir '\.rvm' "$PATH"
-      echo "rvm is already in the \$PATH"
-    else
-      rvm default
-    end
+  set ruby_ver eval (which ruby)
+  if string match -q -- "*.rvm*" $ruby_ver
+    echo "Now switch back to system ruby"
+    rvm use system
   else
-    if set -l index (string match -qir '\.rvm' $PATH)
-      set --erase PATH[$index]
-      echo "removed all instances of rvm from the \$PATH"
-    else
-      echo ".rvm dir was not found in $HOME"
-    end
+    rvm default
   end
 end
