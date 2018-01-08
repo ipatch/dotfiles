aea6bcf919aba2114a2e404537ad7bc1564c4276
ipatch <chris.r.jones.1983@gmail.com>
2018-01-08 00:29:15 -0600

    further refining rvm_toggle

--- config/fish/functions/rvm_toggle.fish
+++ config/fish/functions/rvm_toggle.fish
@@ -1,11 +1,15 @@
 function rvm_toggle --description 'toggle rvm if found'
   set ruby_ver eval (which ruby)
-  if string match -q -- "*.rvm*" $ruby_ver
-    echo "Now switch back to system ruby"
-    rvm use system
-  else if
-    rvm default
+  # switch ($ruby_ver)
+  if string match -q -- "*.rvm*" $ruby_ver #and test -d $HOME/.rvm;
+      echo "Now switch back to system ruby"
+      rvm use system
+  else if string match -q --invert "*.rvm*" $ruby_ver #and test -d $HOME/.rvm;
+      rvm default
   else
-    echo "rvm not found in \$PATH"
+      echo "rvm not found in \$PATH"
   end
 end
+
+# set ruby_ver eval (which ruby)
+# if string match -q -- "*.rvm*" $ruby_ver
