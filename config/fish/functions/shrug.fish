f8f767f832edd1d8629dd62359f53b445beeda83
ipatch <chris.r.jones.1983@gmail.com>
2018-01-21 17:55:58 -0600

    switch from xclip to xsel

--- config/fish/functions/shrug.fish
+++ config/fish/functions/shrug.fish
@@ -1,7 +1,7 @@
 function shrug --description 'add ¯\_(ツ)_/¯ to the system clipboard'
   if type -q pbcopy
     echo -n "¯\_(ツ)_/¯" | pbcopy
-  else if type -q xclip
-    echo -n "¯\_(ツ)_/¯" | pbcopy
+  else if type -q xsel
+    echo -n "¯\_(ツ)_/¯" | xsel --clipboard
   end
 end
