5a343321980ca3f2bbd03b199cc4a8fe93310d73
ipatch <chris.r.jones.1983@gmail.com>
2018-01-21 17:53:03 -0600

    fix syntax errors

--- config/fish/functions/shrug.fish
+++ config/fish/functions/shrug.fish
@@ -1,7 +1,6 @@
 function shrug --description 'add ¯\_(ツ)_/¯ to the system clipboard'
   if type -q pbcopy
     echo -n "¯\_(ツ)_/¯" | pbcopy
-  end
   else if type -q xclip
     echo -n "¯\_(ツ)_/¯" | pbcopy
   end
