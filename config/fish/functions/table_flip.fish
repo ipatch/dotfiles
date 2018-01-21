f6ce690b4f68e9942d03818a56914d9d3fa306f4
ipatch <chris.r.jones.1983@gmail.com>
2018-01-21 17:55:58 -0600

    switch from xclip to xsel

--- config/fish/functions/table_flip.fish
+++ config/fish/functions/table_flip.fish
@@ -1,7 +1,7 @@
 function table_flip --description 'add (╯°□°）╯︵ ┻━┻ to the system clipboard'
   if type -q pbcopy
     echo -n "(╯°□°）╯︵ ┻━┻" | pbcopy
-  else if type -q xclip
-    echo -n "(╯°□°）╯︵ ┻━┻" | xclip
+  else if type -q xsel
+    echo -n "(╯°□°）╯︵ ┻━┻" | xsel --clipboard
   end
 end
