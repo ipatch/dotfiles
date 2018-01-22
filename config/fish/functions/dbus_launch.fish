135c1a1b930e84f4b2811a8c1192f94bc232a9de
ipatch <chris.r.jones.1983@gmail.com>
2018-01-22 14:08:16 -0600

    Bowie's Theorem: 	If an experiment works, you must be using the wrong equipment.

--- config/fish/functions/dbus_launch.fish
+++ config/fish/functions/dbus_launch.fish
@@ -1,8 +1,10 @@
 function dbus_launch --description 'start dbus daemon'
-  if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+  if test -z "$DBUS_SESSION_BUS_ADDRESS"
     # if $DBUS_... string returns zero
     eval 'dbus-launch --auto-syntax --exit-with-session'
     echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
+  else
+    echo "\$DBUS... already set, and is: \n $DBUS_SESSION_BUS_ADDRESS"
   end
 end
 
