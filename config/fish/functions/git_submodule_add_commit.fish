94ee52c8701f65765ff308fdb4a11da1e35364b8
ipatch <chris.r.jones.1983@gmail.com>
2018-02-04 02:38:35 -0600

    update all files

---
+++ config/fish/functions/git_submodule_add_commit.fish
@@ -0,0 +1,4 @@
+function git_submodule_add_commit --description 'git add & commit within submodules'
+  # TODO: get a working function working.
+  # git submodule  foreach --recursive  |  tail  -r | sed 's/Entering//' | xargs -I% cd % ; git add -A and git commit
+end
