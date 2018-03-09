cad2482dbfee6f8f638aee2603eb784689cd47a3
ipatch <chris.r.jones.1983@gmail.com>
2018-03-08 22:24:15 -0600

    The wonderful thing about a dancing bear is not how well he dances, but that he dances at all.

--- config/fish/functions/gittree.fish
+++ config/fish/functions/gittree.fish
@@ -8,20 +8,22 @@ function gittree --description "Recursively traverse git repositories, with supp
     return 1
   end
 
-  for gitdir in (find . -type d -name .git); do
+  for gitdir in (find . -type d -name .git)
     # Display repository name in blue
-    repo=(dirname $gitdir)
-    echo -e "\033[34m$repo\033[0m"
+    set -l repo (dirname $gitdir)
+    # echo -e "\033[34m$repo\033[0m" # ANSCI C colors 👎
+    echo (set_color blue)$repo(set_color normal)
 
     # Run git command in the repositories directory
-    cd $repo; and git $@;
-    ret=$?
+    cd $repo; and git $argv[1]
+    # ret=$? #BASHism
+    set -l ret $status
 
     # Return to calling directory (ignore output)
     cd - > /dev/null
 
     # Abort if `cd` or `git` command fails
-    if [ $ret -ne 0 ];
+    if [ $ret -ne 0 ]
       return 1
     end
 
