f1df564074ff4faf8fd9e755409cd51469e838ac
ipatch <chris.r.jones.1983@gmail.com>
2018-03-13 01:41:27 -0500

    In case of atomic attack, all work rules will be temporarily suspended.

--- terms/tmux/bin/dot.sh
+++ terms/tmux/bin/dot.sh
@@ -11,3 +11,17 @@ tmux send-keys -t dot 'nvim' C-m
 
 # split window
 tmux split-window -v -t dot
+
+# create a new tmux console window
+tmux new-window -n console -t dot
+
+# `cwd` to `$dotfiles`
+tmux send-keys -t dot:2 'cd $dotfiles'C-m
+
+# make `editor` the active window
+tmux select-window -t dot:1
+
+# attach to this newly built tmux session
+tmux attach -t dot
+
+
