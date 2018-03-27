f3a215522d5e5c4dc0bbdbe6ee36930d63d6dfb0
ipatch <chris.r.jones.1983@gmail.com>
2018-03-27 15:36:16 -0500

    add clarity to `docker.fish`

--- config/fish/conf.d/docker.fish
+++ config/fish/conf.d/docker.fish
@@ -1,4 +1,4 @@
 if type -q docker-machine
   # eval (docker-machine env default) # <= avoid `eval` too much overhead!
-  docker-machine env default | source -
+  docker-machine env default | source - # `source -` will source the current file
 end
