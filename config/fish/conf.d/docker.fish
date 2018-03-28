a73cf5f6b83261231d016e0f20c658ccd6ce6f67
ipatch <chris.r.jones.1983@gmail.com>
2018-03-27 20:08:47 -0500

    more robust `docker.fish` check / script

--- config/fish/conf.d/docker.fish
+++ config/fish/conf.d/docker.fish
@@ -1,4 +1,5 @@
 if type -q docker-machine
+  and docker-machine ls | string match -r -q -- '^default '
   # eval (docker-machine env default) # <= avoid `eval` too much overhead!
   docker-machine env default | source - # `source -` will source the current file
 end
