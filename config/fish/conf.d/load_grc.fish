2aa2114d44ade566572d3e2897cb23ebc6516485
ipatch <chris.r.jones.1983@gmail.com>
2018-04-03 15:59:44 -0500

    The only way to amuse some people is to slip and fall on an icy pavement.

--- config/fish/conf.d/load_grc.fish
+++ config/fish/conf.d/load_grc.fish
@@ -1,7 +1,10 @@
 switch (uname)
   case Linux
     if type -q grc
+      echo 'we have grc'
       source /usr/local/etc/grc.fish
+    else
+      echo 'where the hell is grc'
     end
   case Darwin
     if type -q grc
