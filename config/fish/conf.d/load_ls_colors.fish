3eed3da896f2a86d199bcf822958c2d607a5c738
ipatch <chris.r.jones.1983@gmail.com>
2018-04-03 15:41:16 -0500

    update `load_ls_colors_fish` script

--- config/fish/conf.d/load_ls_colors.fish
+++ config/fish/conf.d/load_ls_colors.fish
@@ -3,5 +3,18 @@ switch (uname)
     # TODO: setup a more fancy color scheme for `ls`
     set -gx CLICOLOR yes
   case Linux
-    set -gx LS_COLORS 
+    if not set -q LS_COLORS
+      if command -s dircolors >/dev/null
+        set -l colorfile
+        for file in ~/.dir_colors ~/.dircolors /etc/DIR_COLORS
+          if test -f $file
+            set colorfile $file
+            break
+          end
+        end
+        # Here we rely on the legacy behavior of `dircolors -c` producing output suitable for
+        # csh in order to extract just the data we're interested in.
+        set -gx LS_COLORS (dircolors -c $colorfile | string split ' ')[3]
+      end
+    end
 end
