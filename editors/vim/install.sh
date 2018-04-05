29de717c22f62954e49578ce69614090e974b9f4
ipatch <chris.r.jones.1983@gmail.com>
2018-04-04 23:51:50 -0500

    begin hashing out `install.sh` script

---
+++ editors/vim/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# Install script for @ipatch dotfiles.
+# version: 0.0.4
+
+# setup some env vars for install script
+GREEN_COLOR="\033[0;32m"
+
+function green_color() {
+  echo -e "${GREEN_COLOR}\c"
+}
+
+
+function welcome() {
+  green_color
+
+}
+
+
+
+
