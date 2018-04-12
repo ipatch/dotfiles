5f8b437856a9df4599295425cd26fb847baba846
ipatch <chris.r.jones.1983@gmail.com>
2018-04-12 16:15:15 -0500

    refractor `rm_ds_store_files.sh` and `com.chrisrjones.rm_ds_store_files.plist` to play nice 👨‍❤️‍💋‍👨

--- jobs/bin/rm_ds_store_files.sh
+++ jobs/bin/rm_ds_store_files.sh
@@ -5,7 +5,12 @@
 # To get a list all of `.DS_Store` files on the local system
 #... `sudo -E ls -R / | grep "\.DS_Store"`
 
-sudo -E find / -name .DS_Store -exec rm -f {} \; -print
+# Stupid 💩 to hopefully get launchd / launchctl to play nice
+/bin/launchctl setenv PATH /Users/capin/.local/bin
+
+/usr/local/bin/find / -name .DS_Store -exec /bin/rm -rf {} \; -print
+
+/bin/launchctl unsetenv PATH
 
 
 
