6000ec5a8ad4ffc29774a0ad30fedbb97013c0b7
ipatch <chris.r.jones.1983@gmail.com>
2018-04-27 08:54:36 -0500

    add 'up arrow' / ↑ key binding to merge history between panes when using tmux

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,10 @@
 function fish_user_key_bindings
   fzf_key_bindings
+
+  if [ $TERM = tmux-256color ]
+    # bind -k up 'history --merge ; up-or-search'
+    bind \e\[A 'history --merge ; up-or-search'
+  else
+    # DON'T merge history
+  end
 end
