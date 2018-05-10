49bbce0e72e19e6be3a4f4fe6534300332ee94cb
ipatch <chris.r.jones.1983@gmail.com>
2018-05-09 21:52:55 -0500

    update `006_load_docker.fish` to load the default docker vm, ie. `default-xhyve` for local macOS dev box

--- config/fish/conf.d/006_load_docker.fish
+++ config/fish/conf.d/006_load_docker.fish
@@ -1,16 +1,19 @@
-if type -q docker-machine
-  and docker-machine ls | string match -r -q -- '^default '
-  # eval (docker-machine env default) # <= avoid `eval` too much overhead!
+switch (uname)
+case Darwin
+  if type -q docker-machine
+  and docker-machine ls | string match -r -q -- '^default-xhyve '
+    # eval (docker-machine env default) # <= avoid `eval` too much overhead!
 
-  # perform some check to see if docker is running to avoid the,
-  # `Error checking TLS connection: Host is not running`
-  # `string match -i` = --ignore-case
-  # `string match -r` = Perl-compatible regex
-  if docker-machine ls | string match -i -r -q ' running '
-    # DO NOTHING
-    docker-machine env default | source - # setup default env for fish shell.
-  else
-    docker-machine start default
-    docker-machine env default | source - # `source -` will source the current file
+    # perform some check to see if docker is running to avoid the,
+    # `Error checking TLS connection: Host is not running`
+    # `string match -i` = --ignore-case
+    # `string match -r` = Perl-compatible regex
+    if docker-machine ls | string match -i -r -q ' running '
+      # DO NOTHING
+      docker-machine env default-xhyve | source - # setup default env for fish shell.
+    else
+      docker-machine start default-xhyve
+      docker-machine env default-xhyve | source - # `source -` will source the current file
+    end
   end
 end
