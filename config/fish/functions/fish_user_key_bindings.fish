1cba1fb57979fcfcb773a1e372feb2c582ec9442
truckmonth <me@chrisrjones.com>
2018-06-11 12:31:58 -0500

    update keybindings for fish

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,9 @@ function fish_user_key_bindings
   # fzf_key_bindings
 
   # User provided key bindings
+  # NOTE: `\e` is the "meta" key, ie. ⌘  on macOS
   bind \ev 'fish_clipboard_paste'
+  bind \ec 'fish_clipboard_copy'
 
   if [ $TERM = tmux-256color ]
     # bind -k up 'history --merge ; up-or-search'
