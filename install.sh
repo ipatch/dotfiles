b6f98044d1fbeb582b13e0d7e6ffef2d93831da0
ipatch <chrisr.r.jones.1983@gmail.com>
2018-07-12 12:00:31 -0500

    add install instructions for `install.sh`

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@
 # Install script for @ipatch dotfiles.
 # version: 0.0.5
 echo "more wtfz"
+
+#############################
+# INSTALLATION
+##
+# curl -s https://raw.githubusercontent.com/ipatch/dotfiles/feature-exp-with-curl/develop/install.sh | /bin/sh
