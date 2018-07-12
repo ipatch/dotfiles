16fe0770db7647bac6367c7a1f0b6c026c2f8591
ipatch <chrisr.r.jones.1983@gmail.com>
2018-07-12 10:47:43 -0500

    donking around with `install.sh`

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/sh
 # Install script for @ipatch dotfiles.
 # version: 0.0.4
+
+##############################
+# PREAMBLE
+##
+# `curl`
+  # -f = fail silently
+  # -s = silent mode, ie. no progress bar
+  # -S = works in conjunction with `-s` and shows errors if something fails "F-"
+  # -L = useful for redirection
+##
+
 
 # setup some env vars for install script
 GREEN_COLOR="\033[0;32m"
