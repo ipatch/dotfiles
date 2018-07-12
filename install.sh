6db692a5cdb4880c872cea3d863ead0013f90612
ipatch <chrisr.r.jones.1983@gmail.com>
2018-07-12 11:47:01 -0500

    finally understanding 🙄 how to curl a file and run it

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+# GitHub: @ipatch
+# Twitter: @truckmonth
+# Email: me@chrisrjones.com
 # Install script for @ipatch dotfiles.
-# version: 0.0.4
+# version: 0.0.5
+
+##############################
+# INSTALLATION
+##
+# curl -s https://raw.githubusercontent.com/ipatch/dotfiles/feature-exp-with-curl/develop/install.sh | /bin/sh
 
 ##############################
 # PREAMBLE
