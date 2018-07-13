34b5a13b18fdcc38fe251d5344838910845d1edc
ipatch <chrisr.r.jones.1983@gmail.com>
2018-07-13 01:38:10 -0500

    add a function for BASH to status / print 256 color space

---
+++ config/bash/functions/print_256_colors.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function print_256_colors() {
+ for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done
+}
+
+function print_8bit_color() {
+ for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done
+}
+
