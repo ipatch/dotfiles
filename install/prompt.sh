01bf9a7a80ab3a464bc700e936b5ac5384df006f
ipatch <chris.r.jones.1983@gmail.com>
2018-07-18 19:43:39 -0500

    expiermenting with user input

--- install/prompt.sh
+++ install/prompt.sh
@@ -10,13 +10,41 @@ set -e
 #   echo No
 # fi
 
-echo "Is this a good question (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty "$old_stty_cfg"
-if echo "$answer" | grep -iq "^y" ;then
-    echo Yes
+# echo "Is this a good question (y/n)? "
+# old_stty_cfg=$(stty -g)
+# stty raw -echo
+# answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+# stty "$old_stty_cfg"
+# if echo "$answer" | grep -iq "^y" ;then
+#     echo Yes
+# else
+#     echo No
+# fi
+
+# [[ -t 0 ]] &&
+# read -n 1 -p $'\e[1;32m
+# Do xxxx? (Y/n)\e[0m ' do_xxxx
+# if [[ $do_xxxx =~ ^(y|Y|)$ ]]
+# then
+#   xxxx
+# fi
+
+# while true; do
+#   read -rp "Ask question" yn
+#   case $yn in 
+#     [Yn]* ) okay; break;;
+#     [Nn]* ) exit;;
+#     * ) echo "Yes or No.";;
+#   esac
+# done
+
+echo Would you like to proceed? "(Y or N)"
+
+read -r x
+
+#now check if $x is "y"
+if [ "$x" = "y" ]; then
+  echo "yup"
 else
-    echo No
+  echo "nope"
 fi
