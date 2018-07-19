8fef32c5df6151e2e57eb518d10c8e2e529e2718
ipatch <chris.r.jones.1983@gmail.com>
2018-07-18 19:34:09 -0500

    expiermenting with user input

--- install/prompt.sh
+++ install/prompt.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 set -e
 
+# echo "Is this a good question (y/n)? "
+# old_stty_cfg=$(stty -g)
+# stty raw -echo; answer=$(head -c 1) ; stty "$old_stty_cfg"
+# if echo "$answer" | grep -iq "^y"; then
+#   echo Yes
+# else
+#   echo No
+# fi
+
 echo "Is this a good question (y/n)? "
 old_stty_cfg=$(stty -g)
-stty raw -echo; answer=$(head -c 1) ; stty "$old_stty_cfg"
-if echo "$answer" | grep -iq "^y"; then
-  echo Yes
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty "$old_stty_cfg"
+if echo "$answer" | grep -iq "^y" ;then
+    echo Yes
 else
-  echo No
+    echo No
 fi
