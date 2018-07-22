0627f6667d193888d078ca70e3eb9569a5df52bb
ipatch <chris.r.jones.1983@gmail.com>
2018-07-22 15:08:02 -0500

    update $blogs to $brew_logs

--- jobs/bin/macos-bp-ln.sh
+++ jobs/bin/macos-bp-ln.sh
@@ -3,21 +3,33 @@ set -e
 
 # A simple script to symlink a Bus Pirate to a reasonable name within the `/dev` dir on macOS
 
-if [ -c /dev/tty.usbserial-A700dzDq ]
-then
-  # echo "found bus pirate"
-  sudo ln -sf /dev/tty.usbserial-A700dzDq /dev/buspirate
-  osascript -e 'display notification "Bus Pirate connected via USB" with title "Bus Pirate"' 
-elif [ ! -c /dev/tty.usbserial-A700dzDq ]
-then
-  # echo "no bp found"
-  osascript -e 'display notification "No Bus Pirate found 🤷" with title "Bus Pirate"'
-else
-  # echo "no bus pirate"
-  if [ -h /dev/buspirate ]
-  then
-    sudo rm -r /dev/buspirate
-    osascript -e 'display notification "Bus Pirate disconnected via USB" with title "Bus Pirate"'
-  fi
-fi
+os="$(uname)"
+
+case $os in
+  Darwin)
+    if [ -c /dev/tty.usbserial-A700dzDq ]
+    then
+      # echo "found bus pirate"
+      sudo ln -sf /dev/tty.usbserial-A700dzDq /dev/buspirate
+      osascript -e 'display notification "Bus Pirate connected via USB" with title "Bus Pirate"' 
+    elif [ ! -c /dev/tty.usbserial-A700dzDq ]
+    then
+      # echo "no bp found"
+      osascript -e 'display notification "No Bus Pirate found 🤷" with title "Bus Pirate"'
+    else
+      # echo "no bus pirate"
+      if [ -h /dev/buspirate ]
+      then
+        sudo rm -r /dev/buspirate
+        osascript -e 'display notification "Bus Pirate disconnected via USB" with title "Bus Pirate"'
+      fi
+    fi
+    ;;
+  Linux)
+    echo "use a udev rule for this shit"
+    ;;
+  *)
+    echo "no soup 🍜 for you, come back in 1 year"
+    ;;
+esac
 
