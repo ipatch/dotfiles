20f4947ebaa9846df2ad1748616ca3e0c25a501c
ipatch <chris.r.jones.1983@gmail.com>
2018-07-31 16:05:29 -0500

    migrate `l` to alias from abbr

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -4,9 +4,11 @@
 switch $os
   case Darwin
     #DO NOTHING
+    alias l="ls -lahF --group-directories-first"
   case Linux
     alias ls="ls --color=auto"
-    alias l="ls -lah --color=auto"
+    # alias l="ls -lah --color=auto"
+    alias l="ls -lahF --group-directories-first"
   case *
     ################
     # preserve the $USER environment when running the sudo command.
