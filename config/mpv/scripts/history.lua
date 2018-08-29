ae5702d02684853ca85fbb0c336208e445156d6e
ipatch <chris.r.jones.1983@gmail.com>
2018-08-29 12:00:21 -0500

    think I fixed `history.lua` script

--- config/mpv/scripts/history.lua
+++ config/mpv/scripts/history.lua
@@ -1,13 +1,12 @@
 local HISTFILE = os.getenv("APPDATA") or os.getenv("HOME") .. "/.config/mpv/mpv_history.log"
 
 mp.register_event("file-loaded", function()
-    local title, fp;
+    local title, logfile;
 
     title = mp.get_property("media-title");  
-    title = (title == mp.get_property("filename") and "" or (" (%s)"):format(title));
-    -- title = ''; -- uncomment here
+    title = (title == mp.get_property("filename") and "" or ("(%s)"):format(title));
 
-    fp = io.open(HISTFILE, "a+");
-    fp:write(("[%s] %s%s\n"):format(os.date("%Y-%m-%d %X"), mp.get_property("path"), title));    
-    fp:close();
-end
+    logfile = io.open(HISTFILE, "a+");
+    logfile:write(("[%s] %s %s\n"):format(os.date("%Y-%m-%d %X"), mp.get_property("path"), title));    
+    logfile:close();
+end)
