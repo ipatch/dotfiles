8fd9b45eed93275be758daacc30e345010095c6a
ipatch <chris.r.jones.1983@gmail.com>
2018-09-04 08:55:09 -0500

    fix some linting errors with `git-large-files.sh`

--- jobs/bin/git-large-files.sh
+++ jobs/bin/git-large-files.sh
@@ -11,7 +11,7 @@
 IFS=$'\n';
 
 # list all objects including their size, sort by size, take top 10
-objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head`
+objects=$(git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head)
 
 echo "All sizes are in kB. The pack column is the size of the object, compressed, inside the pack file."
 
@@ -23,11 +23,11 @@ do
 	# extract the compressed size in bytes
 	compressedSize=$((`echo $y | cut -f 6 -d ' '`/1024))
 	# extract the SHA
-	sha=`echo $y | cut -f 1 -d ' '`
+  sha=$(echo "$y" | cut -f 1 -d ' ')
 	# find the objects location in the repository tree
-	other=`git rev-list --all --objects | grep $sha`
+  other=$(git rev-list --all --objects | grep "$sha")
 	#lineBreak=`echo -e "\n"`
-	output="${output}\n${size},${compressedSize},${other}"
+	output="${output}""\\n""${size},${compressedSize},${other}"
 done
 
-echo -e $output | column -t -s ', '
+echo -e "$output" | column -t -s ', '
