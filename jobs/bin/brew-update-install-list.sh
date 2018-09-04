fb74a8d5f777614c70ec3f15fc4a76c12a7ebb58
ipatch <chris.r.jones.1983@gmail.com>
2018-09-03 20:04:06 -0500

    add bash script to update list of formula installed with brew

---
+++ jobs/bin/brew-update-install-list.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# NOTE: a simple bash script to update a list of formula installed via homebrew on macOS / Linux
+
+set -e
+
+if [ "$(type -a brew)" ]; then
+  # echo "brew found"
+  dots_os="$(/usr/bin/uname)"
+
+  case $dots_os in
+    Darwin)
+      echo "your on macOS"
+      if [ -z ${dots+x} ]; then 
+        echo "dots env var has not been set";
+        dots="/opt/Code/dotfiles";
+        echo "set dots env var to '$dots'";
+      else
+        echo "dots env var has been set to '$dots'";
+      fi
+
+      cd $dots/config/brew/macOS/10.13/
+      brew bundle --force dump
+      ;;
+    Linux)
+      echo "your on Linux"
+      if [ -z ${dots+x} ]; then 
+        echo "dots env var has not been set";
+        dots="/opt/Code/dotfiles";
+        echo "set dots env var to '$dots'";
+      else
+        echo "dots env var has been set to '$dots'";
+      fi
+
+      cd $dots/config/brew/Debian/stretch
+      brew bundle --force dump
+      ;;
+    *)
+      echo "sorry can't help yuh 🤷"
+      ;;
+  esac
+fi
