3eca2991f80909e7bae1b196f4ca78ae201bd837
Chris Jones <chris.r.jones.1983@gmail.com>
2018-09-15 14:32:06 -0500

    completely overhauled com.chrisrjones launchd plist scripts

---
+++ jobs/bin/brew-update-Brewfile.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# NOTE: a simple bash script to update a list of formula installed via homebrew on macOS / Linux
+# NOTE: thank you 🙏 very much http://launched.zerowidth.com/
+# NOTE: this script should be called via a launchd service file that runs every Thursday at 11PM
+
+set -e
+
+path_to_brew=$(command -v brew)
+if [ -x "$path_to_brew" ]; then
+  echo "brew found"
+  dots_os="$(/usr/bin/uname)"
+
+  case $dots_os in
+    Darwin)
+      # setup some env vars for launchd / launchctl
+      /bin/launchctl setenv USER "$(whoami)"
+      echo "USER env var equals $USER"
+      /bin/launchctl setenv HOME "/Users/$USER"
+      echo "your on macOS"
+      echo "HOME evn var equals $HOME"
+      if [ -z ${dots+x} ]; then 
+        echo "dots env var has not been set";
+        dots="/opt/Code/dotfiles";
+        echo "set dots env var to '$dots'";
+      else
+        echo "dots env var has been set to '$dots'";
+      fi
+
+      cd $dots/config/brew/macOS/10.13/
+      echo "$PATH";
+      brew bundle --force dump
+
+      # tear down env vars for launchd / launchctl
+      /bin/launchctl unsetenv HOME
+      /bin/launchctl unsetenv USER
+      /bin/launchctl unsetenv PATH
+      ;;
+    Linux)
+      echo "your on Linux"
+      if [ -z ${dots+x} ]; then 
+        echo "dots env var has not been set";
+        dots="/opt/Code/dotfiles";
+        echo "set dots env var to '$dots'";
+      else
+        echo "dots env var has been set to '$dots'";
+      fi
+
+      cd $dots/config/brew/Debian/stretch
+      brew bundle --force dump
+      ;;
+    *)
+      echo "sorry can't help yuh 🤷"
+      ;;
+  esac
+fi
+
+
