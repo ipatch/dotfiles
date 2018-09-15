d2abc39845c5919d9c27752194b30ff819f38e36
Chris Jones <chris.r.jones.1983@gmail.com>
2018-09-15 14:32:02 -0500

    completely overhauled com.chrisrjones launchd plist scripts

--- jobs/bin/rm_ds_store_files.sh
+++ jobs/bin/rm_ds_store_files.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-
+set -e
 # Remove those pesky `.DS_Store` files on macOS
 
 # To get a list all of `.DS_Store` files on the local system
 #... `sudo -E ls -R / | grep "\.DS_Store"`
 
-# Stupid 💩 to hopefully get launchd / launchctl to play nice
-/bin/launchctl setenv PATH /Users/capin/.local/bin
-
 /usr/local/bin/find / -name .DS_Store -exec /bin/rm -rf {} \; -print
-
-/bin/launchctl unsetenv PATH
-
-
 
