d0e8f15a3252fb00c0bd3245780828c923d88137
Chris Jones <chris.r.jones.1983@gmail.com>
2018-11-13 15:20:59 -0600

    basic desktop icon display toggle script for macOS

--- jobs/bin/mac_toggle_desktop_icons.sh
+++ jobs/bin/mac_toggle_desktop_icons.sh
@@ -15,8 +15,22 @@ elif [ -x /bin/uname ]; then
 fi
 
 if [ "$os" = "Darwin" ]; then
-  echo "yup your probably using macOS"
+  # echo "yup your probably using macOS"
+
+  desktop_icon_status=$(defaults read com.apple.finder CreateDesktop)
+
+  # echo "$desktop_icon_status"
+
+  if [ "$desktop_icon_status" = "1" ]; then
+    defaults write com.apple.finder CreateDesktop -bool false;
+    killall Finder;
+    echo "no desktop icons for you!"
+  elif [ "$desktop_icon_status" = "0" ]; then
+    defaults write com.apple.finder CreateDesktop -bool true;
+    killall Finder;
+    echo "Desktop icons should be visible"
+  fi
+
 elif [ "$os" != "Darwin" ]; then
   echo "shit dude, this ain't for you"
 fi
-
