4fbb3d71f3ad568af9e3ed812dc4581f767e9914
Chris Jones <chris.r.jones.1983@gmail.com>
2018-12-31 16:29:34 -0600

    update `rm_ds_store_files.sh` to account for `Permssion denied` messgages

--- jobs/bin/rm_ds_store_files.sh
+++ jobs/bin/rm_ds_store_files.sh
@@ -5,5 +5,5 @@ set -e
 # To get a list all of `.DS_Store` files on the local system
 #... `sudo -E ls -R / | grep "\.DS_Store"`
 
-/usr/local/bin/find / -name .DS_Store -exec /bin/rm -rf {} \; -print
+/usr/local/bin/find / -name .DS_Store -exec /bin/rm -rf {} \; -print 2>&1 | grep -v "Permission denied"
 
