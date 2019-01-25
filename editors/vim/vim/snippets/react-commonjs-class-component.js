54e67803e185ba32a1b0ba1aa482e39f92dddef2
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-24 20:14:59 -0600

    setup some basic snippet files for working with react based projects

---
+++ editors/vim/vim/snippets/react-commonjs-class-component.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Snippet extends React.Component {
+  render () {
+    return(
+      <div>
+        Snippet
+      </div>
+    )
+  }
+}
+
+module.exports = Snippet;
---
+++ editors/vim/vim/snippets/react-es6-class-component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Snippet extends React.Component {
+  render() {
+    return (
+      <div>
+        Snippet &gt; ES6 React class based component
+      </div>
+    )
+  }
+}
+
+export default Snippet
