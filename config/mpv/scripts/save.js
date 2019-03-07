3b57f7bcd9bb6ea7d68cc6a56e2c750b6624f54e
Chris Jones <chris.r.jones.1983@gmail.com>
2019-03-07 12:06:58 -0600

    experimenting with writing mpv scripts in javascript

---
+++ config/mpv/scripts/save.js
@@ -0,0 +1,18 @@
+// ------------------------------------
+// A Javascript / ECMAScript file to get
+// 1. window dimensions, ie. size of window on screen
+// 2. window location, ie. x & y axis of window on screen
+// 3. volume level, ie. volume at 42%
+// ...of current file, and save the settings to be reused for opening a different file.
+// ------------------------------------
+// NOTE: this file can be invoked with a key-binding set within `$XDG_CONFIG_HOME/mpv/input.conf`
+// ex: "<key-binding> script_binding <script_name>"
+// ie: meta-s script_binding ./save.js
+
+
+// get value of volume, ie. 42%, a value between 0% - 100%
+// let volume_value = mp.get_property("volume");
+
+
+// experiment
+print("print to OSD of mpv")
