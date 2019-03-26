dd67d5d9f7c0b3059beda3b5cf0eceeb75ccd04e
Chris Jones <chris.r.jones.1983@gmail.com>
2019-03-26 13:25:19 -0500

    setup `generic-formula.rb` file for scaffolding out a homebrew formula when needed

---
+++ editors/vim/vim/snippets/generic-formula.rb
@@ -0,0 +1,32 @@
+class FormulaName < Formula
+  desc "a brief description of what the formulat does"
+  homepage "https://tld.com/app-name/"
+  url "https://github.com/user/app/archive/version-of-app.tar.gz"
+  sha256 "6500000000000000000000000000000000000000000000000000000000000000"
+  heat "https://github.com/user/app/app-repo.git"
+
+  depends_on "" => :build
+
+  conflicts_with "formula-name"
+
+  def install
+    ENV.append "", "-configure-flag"
+    ENV.append "CFLAGS", ""
+    ENV.append "CFLAGS", "--std=gnu89" if ENV.compiler == :clang
+    
+    # GNU autotools
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug"
+    system "make"
+    system sudo make install
+
+    def caveats; <<~EOS
+      This formula requires blahblah, but can't have blahblah installed.
+    
+    EOS
+    end
+
+    test do
+      # prove that the formual successfully installed, and works as intended
+    end
+  end
