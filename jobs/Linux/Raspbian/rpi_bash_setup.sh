d6cde1faad4914099ff155d8de1bfe6bfdf73fb1
Chris Jones <chris.r.jones.1983@gmail.com>
2019-10-23 14:06:13 -0500

    exp with curl and output into shell vars

--- jobs/Linux/Raspbian/rpi_bash_setup.sh
+++ jobs/Linux/Raspbian/rpi_bash_setup.sh
@@ -4,13 +4,11 @@ set -e
 # Hopefully a simple script for a more sane bash environment
 # ...when working with BASH on Raspbian out of the box.
 
+# TODO
 # 1) check if OS is Raspbian
-# 2) pullin text for /etc/bash.bashrc
-# 3) update /etc/bash.bashrc
+# 2) check if `/etc/bashrc.bash already contains contents of snippet
 
-# possible issues
-# 1) more than likely sudo will be required
-
+# ISSUES
 
 # NOTES
 # it appears Raspbian does not create a `$HOME/.bash_profile` out of the box
@@ -24,6 +22,14 @@ touch "$HOME/.bash_profile"
 
 echo "$HOME./bash_profile created."
 
+# store the about of curl cmd into shell variable
+TMP_BASH_PROFILE_URL="https://raw.githubusercontent.com/ipatch/dotfiles/master/jobs/Linux/Raspbian/home/pi/.bash_profile"
+TMP_BASH_PROFILE=$(curl -L $TMP_BASH_PROFILE_URL)
+
+echo "$TMP_BASH_PROFILE" >> "$HOME/.bash_profile"
+
+echo "updated $HOME/.bash_profile"
+
 curl -s --output "$PWD/snippet_bash.bashrc" "https://raw.githubusercontent.com/ipatch/dotfiles/fall/dev/jobs/Linux/Raspbian/snippet_bash.bashrc" # hide contents of snippet, but download to local file
 
 echo "downloaded snippet_bash.bashrc"
@@ -36,3 +42,5 @@ echo "$(cat $PWD/snippet_bash.bashrc)" | sudo -A sh -c 'cat >> /etc/bash.bashrc'
 
 echo "appended the contents of snippet_bash.bashrc to /etc/bash.bashrc"
 echo "it is safe to remove the local copy of snippet_bash.bashrc with $PWD"
+echo "reload shell"
+exec bash
