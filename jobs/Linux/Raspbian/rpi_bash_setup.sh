744c273d397f4cbdf3746897cdd9bdfbfeec8e20
Chris Jones <chris.r.jones.1983@gmail.com>
2019-10-23 12:21:21 -0500

    ups for `rpi_bash_setup.sh`

--- jobs/Linux/Raspbian/rpi_bash_setup.sh
+++ jobs/Linux/Raspbian/rpi_bash_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # Hopefully a simple script for a more sane bash environment
 # ...when working with BASH on Raspbian out of the box.
@@ -15,4 +16,13 @@
 # it appears Raspbian does not create a `$HOME/.bash_profile` out of the box
 
 # create `$HOME/.bash_profile`
+echo "downloaded 'rpi_bash_setup.sh'"
+echo "executing 'rpi_bash_setup.sh'"
+
 touch "$HOME/.bash_profile"
+
+echo "$HOME./bash_profile created."
+
+curl "https://raw.githubusercontent.com/ipatch/dotfiles/fall/dev/jobs/Linux/Raspbian/snippet_bash.bashrc"
+
+echo "downloaded snippet_bash.bashrc"
