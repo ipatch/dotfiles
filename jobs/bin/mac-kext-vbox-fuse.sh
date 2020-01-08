0206ba862baedf564c11a7ec12b1aef5e6e44edf
Chris Jones <chris.r.jones.1983@gmail.com>
2020-01-07 19:03:03 -0600

    [vbox] [fuse] checkin new file for statusing kernel extensions

---
+++ jobs/bin/mac-kext-vbox-fuse.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function status() {
+    kextstat | grep org.virtualbox.kext > /dev/null 2>&1 ;
+    vbox=$((1-$?))
+    kextstat | grep com.github.osxfuse > /dev/null 2>&1 ;
+    fuse=$((1-$?))
+}
+
+status
+
+if [ "$1" == "vbox" ] ; then
+    if [ $vbox == 1 ] ; then
+    echo "Already vbox"
+    else
+        sudo kextunload -b com.github.osxfuse.filesystems.osxfuse
+        sudo kextutil "/Library/Application Support/VirtualBox/VBoxDrv.kext" -r "/Library/Application Support/VirtualBox"
+        sudo kextutil "/Library/Application Support/VirtualBox/VBoxNetAdp.kext" -r "/Library/Application Support/VirtualBox"
+        sudo kextutil "/Library/Application Support/VirtualBox/VBoxNetFlt.kext" -r "/Library/Application Support/VirtualBox"
+        sudo kextutil "/Library/Application Support/VirtualBox/VBoxUSB.kext" -r "/Library/Application Support/VirtualBox"
+    fi
+elif [ "$1" == "fuse" ] ; then
+    if [ $fuse == 1 ] ; then
+    echo "Already fuse"
+    else
+        sudo kextunload -b org.virtualbox.kext.VBoxUSB -b org.virtualbox.kext.VBoxNetFlt -b org.virtualbox.kext.VBoxNetAdp
+        sudo kextunload -b org.virtualbox.kext.VBoxDrv
+        sudo kextutil /Library/Filesystems/osxfuse.fs/Contents/Extensions/10.12/osxfuse.kext
+    fi
+elif [ "$1" == "status" ] ; then
+    echo "vbox = $vbox"
+    echo "fuse = $fuse"
+else
+    echo "Run $0 <vbox|fuse|status>"
+fi
