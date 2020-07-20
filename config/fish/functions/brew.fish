cb77d3bf1b0dcf9874f2e20fd767ebc218ce3408
Chris Jones <chris.r.jones.1983@gmail.com>
2020-07-20 14:18:53 -0500

    dots: run through weekly chores, bash:add func for catching misspelling, fish:completed `brew.fish` func for catching misspelling

--- config/fish/functions/brew.fish
+++ config/fish/functions/brew.fish
@@ -1,21 +1,16 @@
 function brew --description "catch common misspelling"
-  switch "$argv[1]"
+  switch "$argv[3]"
 
-  # TODO: come up with sane way to check for misspelling of `brew cask outdated --greddy`
+  case cask outdated --greddy
+    # NOVA / NO GO, if contains -- "cask outdated --greddy" $argv
+    # NOVA / NO GO, command "brew cask outdated --greedy"
+    # NOVA / NO GO, set correct_cmd (/usr/local/bin/brew cask outdated --greedy)
+    # NOVA / NO GO,command "$correct_cmd"
+    echo "greddy NOT greddy";
+    echo "================================="
+    command brew cask outdated --greedy
 
-  #   case cask outdated --greddy
-  #     # command brew cask outdated --greedy
-  #     # if contains -- "cask outdated --greddy" $argv
-  #     # command "brew cask outdated --greedy"
-  #     echo "greddy NOT greddy";
-  #     set correct_cmd (/usr/local/bin/brew cask outdated --greedy)
-  #     command "$correct_cmd"
-
-  #   case $argv
-  #     # command $argv
-  #     echo "blah"
-
-    case "*"
-      command brew $argv
-    end
+  case "*"
+    command brew $argv
   end
+end
