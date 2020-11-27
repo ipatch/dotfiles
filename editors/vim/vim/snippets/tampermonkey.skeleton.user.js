470ad38bb948ce20668e4feab63bab2b8dc6f667
Chris Jones <chris.r.jones.1983@gmail.com>
2020-11-27 10:28:19 -0600

    #macos, #launchd, update list of installed machomebrew formula, #violentmonkey, finish scaffold script for editing css using a a `userscript`

--- editors/vim/vim/snippets/tampermonkey.skeleton.user.js
+++ editors/vim/vim/snippets/tampermonkey.skeleton.user.js
@@ -1,29 +1,30 @@
 // ==UserScript==
 // @name        [name.of.script]
-// @namespace   [url]
-// @version     [sem.var]
-// @description [short.synopsis.of.what.script.does]
-// @author      [me]
+// @namespace   Violentmonkey Scripts
 // @match       [url.for.script.act.upon]
 // @grant       none
+// @run-at      [see.API], ie. `document-start`
+// @version     [sem.var], ie. 1.0
+// @author      [me], ie. github.com/ipatch
+// @description skeleton violentmonkey userscript for editing CSS
 // @downloadURL [url], ie. [http://localhost/~$USER/tampermonkey.skeleton.user.js
 // @updateURL   [url], ie. [http://localhost/~$USER/tampermonkey.skeleton.user.js
 // ==/UserScript==
 
+console.log('violentmonkey script loaded');
+
+var styleSheet = "" +
+".css-class {" +
+  "background-color: pink;" +
+  "width: 100%;" +
+  "}" +
+  "}" +
+  "";
+
 (function () {
-  'use strict';
-
-  const style = document.createElement('style');
-
-  style.innerHTML =
-    .target-css-class htmlElement {
-      min-height: 300px; /* normally 100px */
-      max-height: 600px; /* normally 300px */
-    }
-  ;
-
-  document.head.appendChild(style);
+  var s = document.createElement('style');
+  s.type = "text/css";
+  s.innerHTML = styleSheet;
+  (document.head || document.documentElement).appendChild(s);
 })();
 
-
-    
