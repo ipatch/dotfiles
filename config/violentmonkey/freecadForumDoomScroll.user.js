7962eed5c97d1522aee7e22ed05f1a11e683fbf8
Chris Jones <chris.r.jones.1983@gmail.com>
2020-12-12 16:00:49 -0600

    #vm #violentmonkey #js add a simple xmlhttprequest example

--- config/violentmonkey/freecadForumDoomScroll.user.js
+++ config/violentmonkey/freecadForumDoomScroll.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name        freecadweb.org doom scroll 
+// @description to infiniti and beyond
+// @author      github.com/ipatch
 // @namespace   Violentmonkey Scripts
+// @homepageURL https://github.com/ipatch/dotfiles
+// @supportURL  https://github.com/ipatch/dotfiles/issues
+// @icon
 // @match       https://forum.freecadweb.org/*
 // @grant       GM.*
 // @grant       GM_*
 // @run-at      document-end
 // @version     1.0.32
-// @author      github.com/ipatch
-// @description to infiniti and beyond
 // @downloadURL http://localhost/~/capin/freecadForumDoomScroll.user.js
 // @updateURL   http://localhost/~/capin/freecadForumDoomScroll.user.js
-// @supportURL  https://github.com/ipatch/dotfiles/issues
-// @homepageURL https://github.com/ipatch/dotfiles
 // ==/UserScript==
 
 (function () {
@@ -21,9 +22,14 @@
   //
   // REF: https://www.youtube.com/watch?v=Al-ZSuVHHM8 <- debounce explanation
   // REF: https://www.youtube.com/watch?v=T8EYosX4NOo <- intersect observer
+  // REF: https://www.w3schools.com/xml/xml_http.asp
+  // REF: https://code-boxx.com/infinite-page-scroll/
+  // REF: https://medium.com/better-programming/everything-about-xmlhttprequest-in-javascript-8adacc98a209
   //
   // TOOD: setup some auto incrementing function to auto bump ver nums
   // console.log('w00t'); // (un)comment after bumping ver # and test script update load
+  //
+  // NOTE: i believe first topic of thread is indexed at `0` and page 2, ie. post 10 == 11 if index were to start at `1`
 
   // EXAMPLES, tests /*{{{*/
   //------------------------------------
@@ -50,11 +56,40 @@
   unsafeWindow.iVar3 = multiplyES6;
   //*}}}*/
 
+  // XMLHttpRequest example  ----------- {{{
+    // 1. define URL
+    // 2. define request object
+    // 3. open request, ie. use GET or POST
+    // 4. use `onload` function log results
+    // 5. set `responseType` ie. document
+    // 6. send request
+  // EXAMPLE,
+    // let myURL = "https://forum.freecadweb.org/viewtopic.php?f=10&t=13710&start=10";
+    // let myRequest = new XMLHttpRequest();
+    // myRequest.open("GET", myURL);
+    // myRequest.onload = function(){
+    //   console.log(myRequest.response);
+    // }
+    // myRequest.responseType = "document";
+    // myRequest.send();
+  // }}}
+
+
   //---------------------------------------------------
   // BEGIN SCRIPT
   // my mod
   // const actionBar = document.querySelector('action-bar');
   // const actionBar = document.getElementById("page-body").querySelectorAll(".action-bar");
+
+
+  // css
+  const css = `
+  .css-class: {
+    position: relative;
+  }
+  `;
+
+
   let pageFooter = document.getElementById('page-footer');
   unsafeWindow.iVar42 = pageFooter;
 
