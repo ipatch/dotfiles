6afeb101a679992e5e310264cfe432cf2a60fe04
chris <chris.r.jones.1983@gmail.com>
2021-03-25 12:58:30 -0500

    make code comment more terse, #fish #fishshell add file check for `asdf.fish`

--- config/fish/interactive.fish
+++ config/fish/interactive.fish
@@ -2,13 +2,16 @@
 # fish shell - interactive.fish
 ##
 
-# NOTE: abbr(s) and aliases set within the interactive fish files require reinitializing fish or else they will not persist when running `exec fish`
+# NOTE: abbr(s) & aliases require fish reinit or will not persist, `exec fish`
 if test -d $HOME/.config/fish/interactive
   for f in $HOME/.config/fish/interactive/*.fish
     source $f
   end
 
-  source $HOME/.asdf/asdf.fish
+  # check if file exists, if so, then load it
+  if test -f $HOME/.asdf/asdf.fish
+    source $HOME/.asdf/asdf.fish
+  end
 
   # set created & updated files & directories to 664 & 775
   umask 002
