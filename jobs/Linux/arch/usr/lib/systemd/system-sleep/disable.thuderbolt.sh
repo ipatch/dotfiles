50e11a960d1de901c9512ce78a485a5db0331add
chris <chris.r.jones.1983@gmail.com>
2021-04-27 14:19:00 -0500

    update disable.thunderbolt.sh #arch linux sleep/wake script

--- jobs/Linux/arch/usr/lib/systemd/system-sleep/disable.thuderbolt.sh
+++ jobs/Linux/arch/usr/lib/systemd/system-sleep/disable.thuderbolt.sh
@@ -9,7 +9,7 @@ case $1/$2 in
 	post/*)
 		echo "Waking from $2...."
 		# referencing `5.10.31-1-lts` will break, use more robust reference LONGTERM
-		insmod /lib/modules/5.10.31-1-lts/kernel/drivers/thunderbolt/thunderbolt.ko.xz
+		insmod /lib/modules/`uname -r`/kernel/drivers/thunderbolt/thunderbolt.ko.xz
 		;;
 
 esac
