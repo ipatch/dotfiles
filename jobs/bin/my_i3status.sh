e6665c59b7f76b3c99b7b09b76ba649ce9fb7813
chris <chris.r.jones.1983@gmail.com>
2021-07-13 20:20:37 -0500

    #archlinux, #bridge, #i3 #i3status, exp with dipslaying cpu fan speeds in the i3status bar

---
+++ jobs/bin/my_i3status.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# shell script to prepend i3status with more stuff
+
+i3status -c /home/capin/.config/i3status/i3status.conf | while :
+do
+	read line
+	fan1_speed=$(cat /sys/devices/platform/applesmc.768/fan1_output)
+	fan2_speed=$(cat /sys/devices/platform/applesmc.768/fan2_output)
+	echo "L$fan1_speed R$fan2_speed|$line"|| exit 1
+
+done
+
+
+
+
+
+
