5ad23062dbfbbe0bf9a264e469257ecf2909d060
Chris Jones <chris.r.jones.1983@gmail.com>
2022-05-14 12:22:39 -0500

    update bitbar fan-speed install script instructions

--- config/bitbar/fan-speed.5s.sh
+++ config/bitbar/fan-speed.5s.sh
@@ -10,6 +10,10 @@
 # One-liner:
 # curl -LO http://www.eidac.de/smcfancontrol/smcfancontrol_2_4.zip && unzip -d temp_dir_smc smcfancontrol_2_4.zip && cp temp_dir_smc/smcFanControl.app/Contents/Resources/smc /usr/local/bin/smc ; rm -rf temp_dir_smc smcfancontrol_2_4.zip
 
+# alt install using homeberew
+# `brew install smcfancontrol`
+# `ln -sf /Applications/smcFanControl.app/Contents/Resources/smc /usr/local/bin/smc`
+
 declare -a FANS # list available fans with smc -f
 declare -a FAN_LABEL # list available fans with smc -f
 FANS=("L" "R")
