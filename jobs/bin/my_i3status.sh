9d9f07adfd8235e79931d4d54da500fc2e40519e
chris <chris.r.jones.1983@gmail.com>
2023-02-09 14:33:56 -0600

    update i3status config files

--- jobs/bin/my_i3status.sh
+++ jobs/bin/my_i3status.sh
@@ -6,12 +6,7 @@ do
 	read line
 	fan1_speed=$(cat /sys/devices/platform/applesmc.768/fan1_output)
 	fan2_speed=$(cat /sys/devices/platform/applesmc.768/fan2_output)
-	echo "L$fan1_speed R$fan2_speed|$line"|| exit 1
+	echo "L$fan1_speed R$fan2_speed $line"|| exit 1
 
 done
-
-
-
-
-
 
