66f702f9b4c1f2b81a849d7490a27d5a8c4f7a92
chris <chris.r.jones.1983@gmail.com>
2023-02-17 11:44:04 -0600

    #i3 comment out fan speed vars

--- jobs/bin/my_i3status.sh
+++ jobs/bin/my_i3status.sh
@@ -5,8 +5,8 @@ i3status -c /home/capin/.config/i3/i3status.conf | while :
 do
 	read line
 	fan1_speed=$(cat /sys/devices/platform/applesmc.768/fan1_output)
-	fan2_speed=$(cat /sys/devices/platform/applesmc.768/fan2_output)
-	echo "L$fan1_speed R$fan2_speed $line"|| exit 1
-
-done
+# 	fan2_speed=$(cat /sys/devices/platform/applesmc.768/fan2_output)
+# 	echo "L$fan1_speed R$fan2_speed $line"|| exit 1
+#
+# done
 
