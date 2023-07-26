035bb642738a58125ed2eee9bde7f711d714c392
chris <chris.r.jones.1983@gmail.com>
2023-07-26 12:23:43 -0500

    #fish disable global env var, breaks fuzzy search `git add`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -32,7 +32,9 @@ if status is-interactive
   # fish specific env vars
   set -gx fish_greeting ""
   set -gx fish_emoji_width 2 # NOT COMPATIBLE with fish <= 2.7.1
-  set -gx fish_complete_path show_matching
+
+  # NOTE: ipatch, below env var will break fuzzy search for `git add`
+  # set -gx fish_complete_path show_matching
 
   # *nix specific env vars
   set -gx XDG_CONFIG_HOME $HOME/.config
