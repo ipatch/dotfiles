21fcf2f43fbb38b1f41ce8a11adae8acf4e21aaf
chris <chris.r.jones.1983@gmail.com>
2023-08-03 15:32:51 -0500

    #rice #i3 #polybar spent too much ricing this afternoon 😑

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q -s SIGKILL polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
