bbe15cd146318a6afa59f30f8a4771af7599661b
chris <chris.r.jones.1983@gmail.com>
2023-08-03 15:32:16 -0500

    override default mappings with ufo to open/close folds

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -613,6 +613,10 @@ require('ufo').setup({
   end
 })
 
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
 -- NOTE: ipatch Helper function to restore folds
 function RestoreFolds()
   local last_view = vim.fn.winsaveview()
