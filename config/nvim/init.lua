044e368a9aa83b27d2572005dfbb79d89e1cbd97
chris <chris.r.jones.1983@gmail.com>
2023-08-22 12:44:52 -0500

    #nvim add telescope-textobjects setup function mappings, add telescope close buffer mapping

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,7 +67,7 @@ require('packer').startup(function(use)
     }
   })
 
-  -- lsp configuration and plugins
+  -- LSP configuration and plugins
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'dev-v3',
@@ -110,6 +110,12 @@ require('packer').startup(function(use)
     end
   }
   use 'nvim-treesitter/playground'
+
+  use({
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
 
   -- telescope
   use {
@@ -585,25 +591,6 @@ require'lspconfig'.solargraph.setup{
   }
 }
 
--- require("lspconfig").ruby_ls.setup({
---   -- on_attach = function(client, buffer)
---   --   setup_diagnostics(client, buffer)
---   -- end,
---   cmd = { "bundle", "exec", "ruby-lsp" }
--- })
-
-
--- require('mason-lspconfig').setup({
---   handlers = {
---     lsp.default_setup,
---     lua_ls = function()
---       -- NOTE: ipatch, DO NOT CALL `lua_ls.setup` 2x, twice!
---       require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
---     end,
---     
---   }
--- })
-
 -- nvim_lsp.lua_ls.setup({
 --   on_attach = on_attach,
 --   capabilities = capabilities,
@@ -626,7 +613,6 @@ require'lspconfig'.solargraph.setup{
 --     end
 --   end,
 -- })
-
 
 ---------------
 -- PLUGIN / nvim-cmp, neovim completion 
@@ -727,17 +713,26 @@ table.insert(vimgrep_arguments, "--hidden")
 table.insert(vimgrep_arguments, "--glob")
 table.insert(vimgrep_arguments, "!**/.git/*")
 
+local actions = require "telescope.actions"
+
 telescope.setup({
-	defaults = {
-		-- `hidden = true` is not supported in text grep commands.
-		vimgrep_arguments = vimgrep_arguments,
+  defaults = {
+    -- `hidden = true` is not supported in text grep commands.
+    vimgrep_arguments = vimgrep_arguments,
 	},
-	pickers = {
-		find_files = {
-			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-		},
-	},
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+    buffers = {
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer
+        }
+      }
+    },
+  }
 })
 
 local builtin = require('telescope.builtin')
@@ -844,6 +839,93 @@ ts.setup {
       node_incremental = '<c-space>',
       scope_incremental = '<c-s>',
       node_decremental = '<c-backspace>',
+    },
+  },
+}
+
+---------------
+-- PLUGIN / treesitter-textobjects, treesitter, tree-sitter
+----
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+        -- nvim_buf_set_keymap) which plugins like which-key display
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        -- You can also use captures from other query groups like `locals.scm`
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+      },
+      -- You can choose the select mode (default is charwise 'v')
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * method: eg 'v' or 'o'
+      -- and should return the mode ('v', 'V', or '<c-v>') or a table
+      -- mapping query_strings to modes.
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      -- If you set this to `true` (default is `false`) then any textobject is
+      -- extended to include preceding or succeeding whitespace. Succeeding
+      -- whitespace has priority in order to act similarly to eg the built-in
+      -- `ap`.
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * selection_mode: eg 'v'
+      -- and should return true of false
+      include_surrounding_whitespace = true,
+    },
+
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+        ["]o"] = "@loop.*",
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+        ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        -- ["]d"] = "@conditional.outer",
+      },
+      goto_previous = {
+        -- ["[d"] = "@conditional.outer",
+      }
     },
   },
 }
