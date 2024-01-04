1a3c39b6bc310c77925b6b8c9f5ce6dca2fd83f9
chris <chris.r.jones.1983@gmail.com>
2024-01-04 11:13:20 -0600

    #nvim exp osc52 create copy func

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -399,6 +399,14 @@ require('osc52').setup {
   tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
 }
 
+function copy()
+  if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+    require('osc52').copy_register('+')
+  end
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+
 ---------------
 -- PLUGIN / AI / chatgpt.nvim
 ----
