3ccb87210c0158bf76ad74f6ae113c1ffabfa624
chris <chris.r.jones.1983@gmail.com>
2024-01-04 11:18:39 -0600

    #nvim exp osc52 create copy func

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -399,11 +399,30 @@ require('osc52').setup {
   tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
 }
 
-function copy()
-  if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-    require('osc52').copy_register('+')
-  end
+-- NO WORK!
+-- function copy()
+--   if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+--     require('osc52').copy_register('+')
+--   end
+-- end
+
+local function copy(lines, _)
+  require('osc52').copy(table.concat(lines, '\n'))
 end
+
+local function paste()
+  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {['+'] = copy, ['*'] = copy},
+  paste = {['+'] = paste, ['*'] = paste},
+}
+
+-- Now the '+' register will copy to system clipboard using OSC52
+vim.keymap.set('n', '<leader>c', '"+y')
+vim.keymap.set('n', '<leader>cc', '"+yy')
 
 vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
 
