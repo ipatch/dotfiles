4fc7b9041dc390f285487111fd58bf7fe3b45cea
chris <chris.r.jones.1983@gmail.com>
2024-01-05 10:49:19 -0800

    #nvim exp with copying using osc52 through ssh

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -399,6 +399,18 @@ require('osc52').setup {
   tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
 }
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
 -- vim.opt.clipboard = 'unnamedplus'
 -- vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
 -- vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
@@ -421,19 +433,19 @@ require('osc52').setup {
 --   require('osc52').copy(table.concat(lines, '\n'))
 -- end
 
-local function paste()
-  return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-end
-
-vim.g.clipboard = {
-  name = 'osc52',
-  copy = {['+'] = copy, ['*'] = copy},
-  paste = {['+'] = paste, ['*'] = paste},
-}
-
--- Now the '+' register will copy to system clipboard using OSC52
-vim.keymap.set('n', '<leader>c', '"+y')
-vim.keymap.set('n', '<leader>cc', '"+yy')
+-- local function paste()
+--   return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+-- end
+--
+-- vim.g.clipboard = {
+--   name = 'osc52',
+--   copy = {['+'] = copy, ['*'] = copy},
+--   paste = {['+'] = paste, ['*'] = paste},
+-- }
+--
+-- -- Now the '+' register will copy to system clipboard using OSC52
+-- vim.keymap.set('n', '<leader>c', '"+y')
+-- vim.keymap.set('n', '<leader>cc', '"+yy')
 
 -- vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
 
