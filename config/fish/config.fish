8b4324fb363a18c53408cfb1df93040d98afc5b4
chris <chris.r.jones.1983@gmail.com>
2024-04-06 16:08:00 -0500

    #fish config.fish cleanup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -104,12 +104,10 @@ if status is-interactive
   set -gx HOMEBREW_NO_INSTALL_CLEANUP 1
   set -gx HOMEBREW_NO_INSTALL_FROM_API 1
 
-  # TODO: ipatch, setup env var specific for homebrew on asahi linux
   # below is required to run `brew update`
   if uname -a | grep -q "asahi"
     set -gx HOMEBREW_USE_RUBY_FROM_PATH 1
   end
-
 
   # NOTE: ipatch, for reasons above my pay grade the /tmp dir was running out of space on m1 mini running asahi
   #---
