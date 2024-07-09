358dd7b44f3aa367cee2f4b8f1b1313f89ceb1ed
chris <chris.r.jones.1983@gmail.com>
2024-07-09 12:38:47 -0500

    #rofi add notes to my profile.d script

--- jobs/Linux/arch/etc/profile.d/capin_hb_paths.sh
+++ jobs/Linux/arch/etc/profile.d/capin_hb_paths.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# NOTE: ipatch, https://github.com/davatorium/rofi/discussions/1281#discussioncomment-412050
+# cat /proc/$(pidof i3)/environ | tr '\0' '\n' | grep '^PATH'
+
 if [ "$USER" = "capin" ]; then
   export PATH=$HOME/hombrew/sbin:$HOME/homebrew/bin:$PATH
 fi
