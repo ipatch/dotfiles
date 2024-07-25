64eb90074a89c0ee657c5eaa7ddd6062e2bba0aa
chris <chris.r.jones.1983@gmail.com>
2024-07-25 16:00:32 -0500

    #neovim treesitter add some useful links for ruby folds.scm

--- config/nvim/after/queries/ruby/folds.scm
+++ config/nvim/after/queries/ruby/folds.scm
@@ -1,4 +1,6 @@
 ;; extends
+;; NOTE: ipatch, https://github.com/nvim-treesitter/nvim-treesitter/issues/998
+;; NOTE: ipatch, https://github.com/kevinhwang91/nvim-ufo/issues/139
 [
  (array)
  (string_array)
