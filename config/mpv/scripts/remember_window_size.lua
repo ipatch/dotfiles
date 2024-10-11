687856eb29b6d5d78b1971d01ebf2378cf524554
chris <chris.r.jones.1983@gmail.com>
2024-10-11 16:12:31 -0500

    #mpv exp with new example

--- config/mpv/scripts/remember_window_size.lua
+++ config/mpv/scripts/remember_window_size.lua
@@ -9,12 +9,11 @@ local opts = {
 options.read_options(opts)
 
 local function save_window_position()
-  local win_x, win_y = mp.get_property_number("window-x"), mp.get_property_number("window-y")
-  local win_w, win_h = mp.get_property_number("window-width"), mp.get_property_number("window-height")
+  local win_w, win_h = mp.get_property_number("width"), mp.get_property_number("height")
   local fs = mp.get_property_native("fullscreen")
 
-  if win_x and win_y and win_w and win_h then
-    local content = string.format("%d,%d,%d,%d,%s", win_x, win_y, win_w, win_h, tostring(fs))
+  if win_w and win_h then
+    local content = string.format("%d,%d,%s", win_w, win_h, tostring(fs))
     local file = io.open(mp.command_native({"expand-path", opts.save_file}), "w")
     if file then
       file:write(content)
@@ -29,12 +28,10 @@ local function restore_window_position()
     local content = file:read("*all")
     file:close()
 
-    local x, y, w, h, fs = content:match("(-?%d+),(-?%d+),(%d+),(%d+),(%a+)")
-    if x and y and w and h then
-      mp.set_property_number("window-x", tonumber(x))
-      mp.set_property_number("window-y", tonumber(y))
-      mp.set_property_number("window-width", tonumber(w))
-      mp.set_property_number("window-height", tonumber(h))
+    local w, h, fs = content:match("(%d+),(%d+),(%a+)")
+    if w and h then
+      mp.set_property_number("width", tonumber(w))
+      mp.set_property_number("height", tonumber(h))
       mp.set_property_bool("fullscreen", fs == "true")
     end
   end
