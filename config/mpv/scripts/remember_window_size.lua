d2bc05cae9f18cd6d6499aeba3baa27ef79736f5
chris <chris.r.jones.1983@gmail.com>
2024-10-12 15:11:35 -0500

    #i3 #mpv scaffold todo list for lua script

--- config/mpv/scripts/remember_window_size.lua
+++ config/mpv/scripts/remember_window_size.lua
@@ -1,2 +1,11 @@
 -- Save this as restore_window_position.lua in your MPV scripts directory
 
+-- NOTE: ipatch, to get the current window size and placement run, xwininfo
+
+-- NOTE: ipatch, to resize a window using `i3-msg`
+-- i3-msg '[class="mpv"] resize set 2743 720'
+
+-- TODO: using the above xwininfo command get the current size of the window
+-- TODO: using the mpv lua api, use the above i3-msg command to set the window size
+
+
