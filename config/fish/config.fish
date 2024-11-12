ecd4d85452a67e96f3069d6dda2bc3b1abb34f0d
chris <chris.r.jones.1983@gmail.com>
2024-11-12 13:18:34 -0300

    #fish config.fish add tree-sitter #todo item

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -283,6 +283,7 @@ if status is-interactive
   # NOTE: path entries will persist and require manual removal!!!
   #--
 
+  # TODO: tree-sitter it'd nice to be able to have a code fold for paths var
   set -l paths \
     $HOME/homebrew/bin \
     $HOME/homebrew/sbin \
