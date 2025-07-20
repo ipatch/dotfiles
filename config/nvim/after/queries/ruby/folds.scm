d7aeca6a5b6d0ef61f99ed9c0cc228d250dae67d
chris <chris.r.jones.1983@gmail.com>
2025-07-20 15:28:49 -0500

    #neovim add link to gist about setting up a custom fold range, FOR THE LOVE OF GOD 🙏

--- config/nvim/after/queries/ruby/folds.scm
+++ config/nvim/after/queries/ruby/folds.scm
@@ -1,7 +1,11 @@
 ;; extends
 ;; NOTE: ipatch, https://github.com/nvim-treesitter/nvim-treesitter/issues/999
 ;; NOTE: ipatch, https://github.com/kevinhwang92/nvim-ufo/issues/139
+
+;; NOTE: ipatch, https://gist.github.com/lestoni/8c74da455cce3d36eb68
+;; `zf#j` # = num of lines to fold equals life🛟
 [
  (array)
  (string_array)
 ] @fold
+
