f3f3c6b0d406e62db88e1b1c4ce920b31ff50789
chris <chris.r.jones.1983@gmail.com>
2025-07-25 14:11:53 -0500

    #neovim #ruby-lsp update settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -668,6 +668,9 @@ lspconfig.ruby_lsp.setup({
   init_options = {
     formatter = 'standard',
     linters = { 'standard' },
+    enabledFeatures = {
+      bundlerLock = false,
+    },
   },
 })
 
