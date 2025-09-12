116efe328c591fc70f88fb01dba561aa67eb9ddf
chris <chris.r.jones.1983@gmail.com>
2025-09-12 14:40:20 -0700

    #fish allow running of test_256_color from within a cli

--- config/fish/functions/test_256_color.fish
+++ config/fish/functions/test_256_color.fish
@@ -1,8 +1,13 @@
+#!/usr/bin/env fish
+
 function test_256_color --description 'display all 256 terminal colors'
   for code in (seq 0 255)
-    echo -e "\e[38;05;"$code"m $code: Test\e[38m"
+    # echo -e "\e[38;05;"$code"m $code: Test\e[38m"
+    printf "\e[38;5;%sm %3s: Test\e[0m\n" $code $code
   end
 end
+
+test_256_color
 
 # Inspiration:
 # https://github.com/chriskempson/base16-shell/issues/5#issuecomment-21001859
