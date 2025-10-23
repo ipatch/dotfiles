e07ac6a8691310e574133b767314af622f2a056a
chris <chris.r.jones.1983@gmail.com>
2025-10-23 17:20:57 -0500

    #neovim init.lua update config for readability

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -144,7 +144,12 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && '
+          .. 'cmake --build build --config Release && '
+          .. 'cmake --install build --prefix build'
+      }
     }
   },
 
