67efab839db75b5a42b03067ed566c623c9bf9e2
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-05-26 14:00:51 -0500

    #fish #homebrew update env var settings ie. set new env var in order to upgrade homebrew-core ruby

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -189,6 +189,7 @@ if status is-interactive
   # NOTE: TODO: may be a good idea to put a link / reference to the file in the brew src code where env vars are defined
   if uname -a | grep -q "asahi"
     set -gx HOMEBREW_USE_RUBY_FROM_PATH 1
+    set -gx HOMEBREW_NO_SANDBOX_LINUX 1
     export DBUS_SESSION_BUS_ADDRESS='unix:abstract=/tmp/custom_dbus_name,guid=5fe0907d81e722390f1ce02d6033ad76'
     set -gx EDITOR "nvim"
   end
